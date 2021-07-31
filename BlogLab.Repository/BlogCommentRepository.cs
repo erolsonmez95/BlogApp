97db18cb5645e3ae6abac928bd5ba83ea343a59f
erolsonmez95 <erolsonmez1995@gmail.com>
2021-07-31 11:00:44 +0300

    identity added by using .net core identity

--- BlogLab.Repository/BlogCommentRepository.cs
+++ BlogLab.Repository/BlogCommentRepository.cs
@@ -1,12 +1,10 @@
 ﻿using BlogLab.Models.BlogComment;
 using Dapper;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogLab.Repository

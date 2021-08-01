fbccfa04ef80c828487670c7d9a7128c8eac3441
erolsonmez95 <erolsonmez1995@gmail.com>
2021-08-01 22:59:32 +0300

    Startup file configured.

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,10 +1,21 @@
 
+using BlogLab.Identity;
+using BlogLab.Models.Account;
 using BlogLab.Models.Settings;
+using BlogLab.Repository;
+using BlogLab.Services;
+using BlogLab.Web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace BlogLab.Web
 {
@@ -13,6 +24,8 @@ namespace BlogLab.Web
     {
         public Startup(IConfiguration configuration)
         {
+            //clear used to be able use our own claim types
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = configuration;
         }
 
@@ -21,9 +34,59 @@ namespace BlogLab.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
 
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
+            //to register services
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            //to register repositories
+            services.AddScoped<IBlogRepository, BlogRepository>();
+            services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            //to register identity
+            services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+            {
+
+              //opt added to keep passwords easy
+                opt.Password.RequireNonAlphanumeric = false;
+
+
+            }).AddUserStore<UserStore>()
+            .AddDefaultTokenProviders()
+            .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+            //allows use controllers
             services.AddControllers();
+            //cors register
+            services.AddCors();
+
+            //jwt registrattion
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(
+                    options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters {
+                            ValidateIssuer = true,
+                            ValidateAudience=true,
+                            ValidateLifetime=true,
+                            ValidateIssuerSigningKey=true,
+                            ValidIssuer=Configuration["Jwt:Issuer"],
+                            ValidAudience= Configuration["Jwt:Issuer"],
+                            IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                            ClockSkew= TimeSpan.Zero
+                        };
+                    }
+                
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,11 +96,23 @@ namespace BlogLab.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.ConfigureExceptionHandler();
 
             app.UseRouting();
 
+            if (env.IsDevelopment())
+            {
+                app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            }
+            else {
+                //if in production uses those configuration
+                app.UseCors(options => options.WithOrigins("https://ourwebsite.com"));
+            }
+            app.UseAuthentication();
             app.UseAuthorization();
 
+           
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

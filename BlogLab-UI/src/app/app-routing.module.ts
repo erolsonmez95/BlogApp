4a19ad3f43f8f42c5e63d3b771a8b94c8d009894
erolsonmez95 <erolsonmez1995@gmail.com>
2021-08-03 23:20:42 +0300

    auth guard added and app-routing configured

--- BlogLab-UI/src/app/app-routing.module.ts
+++ BlogLab-UI/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { NotFoundComponent } from './componets/not-found/not-found.component';
+import { BlogEditComponent } from './componets/blog-components/blog-edit/blog-edit.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
+import { PhotoAlbumComponent } from './componets/photo-album/photo-album.component';
+import { BlogComponent } from './componets/blog-components/blog/blog.component';
+import { LoginComponent } from './componets/login/login.component';
+import { HomeComponent } from './componets/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'blogs',component:BlogComponent},
+  {path:'blogs/:id',component:BlogComponent},
+  {path:'photo-album',component:PhotoAlbumComponent, canActivate:[AuthGuard]},
+  {path:'dashboard',component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'dashboard/:id',component:BlogEditComponent, canActivate:[AuthGuard]},
+  {path:'not-found',component:NotFoundComponent},
+  {path:'**',redirectTo:'/not-found'}
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

30d930eb023a13946f952040b1ce28000629af58
erolsonmez95 <erolsonmez1995@gmail.com>
2021-08-03 12:18:26 +0300

    services,interceptors, components,auth initiliazed

--- BlogLab-UI/src/app/app.module.ts
+++ BlogLab-UI/src/app/app.module.ts
@@ -5,11 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SummaryPipe } from './pipes/summary.pipe';
+import { BlogComponent } from './componets/blog-components/blog/blog.component';
+import { BlogCardComponent } from './componets/blog-components/blog-card/blog-card.component';
+import { BlogEditComponent } from './componets/blog-components/blog-edit/blog-edit.component';
+import { BlogsComponent } from './componets/blog-components/blogs/blogs.component';
+import { FamousBlogsComponent } from './componets/blog-components/famous-blogs/famous-blogs.component';
+import { CommentBoxComponent } from './componets/comment-componets/comment-box/comment-box.component';
+import { CommentSystemComponent } from './componets/comment-componets/comment-system/comment-system.component';
+import { CommentsComponent } from './componets/comment-componets/comments/comments.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
+import { HomeComponent } from './componets/home/home.component';
+import { LoginComponent } from './componets/login/login.component';
+import { NotFoundComponent } from './componets/not-found/not-found.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { PhotoAlbumComponent } from './componets/photo-album/photo-album.component';
+import { RegisterComponent } from './componets/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryPipe
+    SummaryPipe,
+    BlogComponent,
+    BlogCardComponent,
+    BlogEditComponent,
+    BlogsComponent,
+    FamousBlogsComponent,
+    CommentBoxComponent,
+    CommentSystemComponent,
+    CommentsComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    PhotoAlbumComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
